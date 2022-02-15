e0aab49659aed14eae0b8a84136a7309bbb840a5
Dominic C <45153623+DominicChm@users.noreply.github.com>
2022-02-14 17:59:30 -0800

    Refactor to use multiple channels
    Buggy - schema not working yet....

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import {onMount} from 'svelte'
+    import {onDestroy} from 'svelte';
     import io from "socket.io-client"
     import TitledContainer from "./TitledContainer.svelte";
     import {notifications} from './notifications/notifications.js'
@@ -7,19 +7,23 @@
     import Editor from "./Editor.svelte";
     import DataView from "./DataView.svelte";
     import PlayControls from "./PlayControls.svelte";
+    import RunControl from "./RunControl.svelte";
 
     let count: number = 0
 
-    let state = {
-        runs: []
-    };
-
     const sio = io("http://localhost:3000");
+    onDestroy(() => sio.disconnect());
 
-    sio.on("client_state", (s) => {
-        state = s;
-        console.log(s);
-    });
+    let capabilities = {};
+    sio.on("capabilities", c => capabilities = c);
+
+    let schema = {};
+    sio.on("schema", s => schema = s);
+
+    let playState = {};
+    sio.on("play_state", s => playState = s);
+
+
     sio.on("general_error", (e) => notifications.danger(e, 2000));
 
     let data = {}
@@ -33,25 +37,6 @@
         }, 50);
     });
 
-    function initRun(uuid) {
-        sio.emit("run_init", uuid);
-    }
-
-    function stopRun(uuid) {
-        sio.emit("run_stop", uuid);
-    }
-
-    function deleteRun(uuid) {
-        sio.emit("run_delete", uuid);
-    }
-
-    function activateRun(uuid) {
-        sio.emit("activate_run", uuid)
-    }
-
-    function deactivateRun() {
-        sio.emit("deactivate_run");
-    }
 
     function createModule() {
         sio.emit("create_module", "brake_pressure", "AA:BB:CC:DD:EE:FF");
@@ -73,55 +58,6 @@
         font-family: Arial, Helvetica, sans-serif;
     }
 
-    table, th, td {
-        border: 1px solid;
-        border-collapse: collapse;
-    }
-
-    table {
-        border: 1px solid #1C6EA4;
-        background-color: #EEEEEE;
-        width: 100%;
-        text-align: left;
-        border-collapse: collapse;
-    }
-
-    table td, table th {
-        border: 1px solid #AAAAAA;
-        padding: 3px 2px;
-    }
-
-    table tbody td {
-        font-size: 13px;
-    }
-
-    table tr:nth-child(even) {
-        background: #D0E4F5;
-    }
-
-    .activated-run {
-        background: #7ce089 !important;
-    }
-
-    table thead {
-        background: #1C6EA4;
-        background: -moz-linear-gradient(top, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
-        background: -webkit-linear-gradient(top, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
-        background: linear-gradient(to bottom, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
-        border-bottom: 2px solid #444444;
-    }
-
-    table thead th {
-        font-size: 15px;
-        font-weight: bold;
-        color: #FFFFFF;
-        border-left: 2px solid #D0E4F5;
-    }
-
-    table thead th:first-child {
-        border-left: none;
-    }
-
     .vert-div {
         width: 1px;
         margin: 3px 0;
@@ -133,57 +69,18 @@
 
 <div class="App">
     <TitledContainer title="Capabilities">
-        <p>{JSON.stringify(state.capabilities, null, 3)}</p>
+        <p>{JSON.stringify(capabilities, null, 3)}</p>
     </TitledContainer>
     <TitledContainer title="playControls">
         <PlayControls sio={sio}/>
     </TitledContainer>
     <TitledContainer title="Runs">
-        <p>Active: {state.activeRun}
-            <button on:click={() => deactivateRun()}>Deactivate</button>
-        </p>
-        <table>
-            <thead>
-            <tr>
-                <th>UUID</th>
-                <th>Type</th>
-                <th>Locked</th>
-                <th>Size</th>
-                <th>Actions</th>
-                <th>Activate</th>
-            </tr>
-            </thead>
-            {#each state.runs as run}
-                <tr class:activated-run={state.activeRun === run.uuid}>
-                    <td>{run.uuid}</td>
-                    <td>{run.type}</td>
-                    {#if (run.type === "realtime")}
-                        <td></td>
-                        <td></td>
-                        <td>
-                            <button on:click={() => initRun(run.uuid)}>Init Storage</button>
-                        </td>
-                    {:else if (run.type === "stored")}
-                        <td>{run.locked}</td>
-                        <td>{run.size}</td>
-                        <td>
-                            <button on:click={() => deleteRun(run.uuid)}>Delete</button>
-                            {#if (run.locked)}
-                                <button on:click={() => stopRun(run.uuid)}>Stop</button>
-                            {/if}
-                        </td>
-                    {/if}
-                    <td>
-                        <button on:click={() => activateRun(run.uuid)}>Activate</button>
-                    </td>
-                </tr>
-            {/each}
-        </table>
+        <RunControl sio={sio}/>
     </TitledContainer>
     <TitledContainer title="Data">
         <div style="background: {frameIndColor}; width: 10px; height: 10px; border: 1px solid black; margin: 8px"></div>
-        <DataView schema={state.schema} data={data}/>
-        {#if (state.playState?.playing)}
+        <DataView schema={schema} data={data}/>
+        {#if (playState?.playing)}
             <button on:click={handleStopPlay}>Stop Playing</button>
         {:else }
             <button on:click={handlePlay}>Play</button>
@@ -192,7 +89,7 @@
     <button on:click={createModule}>Add Module</button>
     <Editor sio={sio}/>
     <TitledContainer title="Schema">
-        <pre>{JSON.stringify(state.schema, null, 2)}</pre>
+        <pre>{JSON.stringify(schema, null, 2)}</pre>
         <button on:click={createModule}>Add Module</button>
     </TitledContainer>
     <meter value="50" max="100" min="0" low="25" high="75" optimum="50"></meter>
